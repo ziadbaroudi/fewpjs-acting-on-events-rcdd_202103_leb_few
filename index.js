ac2efb97ce6617a22595056711697963b22b172a
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-14 01:30:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ document.addEventListener("keydown", function(e){
   }
 });
 
+document.addEventListener("keydown", function(e){
+  if (e.key === "ArrowRight") {
+    let leftNumbers = dodger.style.left.replace("px", "")
+    let left = parseInt(leftNumbers, 10);
+    if(left<400){dodger.style.left = `${left +1}px`;}
+  }
+});
+
