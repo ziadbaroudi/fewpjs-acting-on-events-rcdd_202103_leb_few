772dde00855d3cd3cc809dbdb9e24410964cba08
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-14 01:21:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+
+let dodger = document.getElementById("dodger");
+
+document.addEventListener("keydown", function(e){
+  if (key === "ArrowLeft") {
+    let leftNumbers = dodger.style.left.replace("px", "")
+    let left = parseInt(leftNumbers, 10);
+    
+    dodger.style.left = `${left -1}px`;
+  }
+});
